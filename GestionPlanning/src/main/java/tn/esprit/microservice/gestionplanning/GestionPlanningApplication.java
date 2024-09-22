6bccfbc2310a7f5105450786d61f70c358cfcd46
Achref <achref.maarfi0@gmail.com>
2024-09-22 19:40:01 +0100

    init

---
+++ GestionPlanning/src/main/java/tn/esprit/microservice/gestionplanning/GestionPlanningApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.microservice.gestionplanning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionPlanningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionPlanningApplication.class, args);
+	}
+
+}
